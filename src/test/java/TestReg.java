30b7ec89d2848dd4d1e3189af20564dc89a76f7e
zheng sen he <61687266+meteorOSS@users.noreply.github.com>
2024-02-19 17:36:48 +0800

    style

--- src/test/java/TestReg.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class TestReg {
-    @Test
-    public void test(){
-        String format = "@59f186785e4da8c84bdb4b1d09384714a86d647ba371160b08f4b961b2bbc4a3:<br/>差不多，磨盘变成了键盘";
-        System.out.println(format.replaceAll("@([^:])+:<br/>",""));
-    }
-}
