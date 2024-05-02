798a4f06393f799b1a3d761406412659ec92c072
zheng sen he <61687266+meteorOSS@users.noreply.github.com>
2024-05-02 20:47:05 +0800

    style: update version

--- src/main/java/com/meteor/wechatbc/util/VersionCheck.java
+++ src/main/java/com/meteor/wechatbc/util/VersionCheck.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class VersionCheck {
 
-    private static final String CURRENT_VERSION = "v1.1.8";
+    private static final String CURRENT_VERSION = "v1.2";
 
     public static void check(String owner, String repo) {
 
